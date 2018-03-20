07651241b74710f2c005e187842da158687e9717
Anil Patel <ajpatel92@icloud.com>
2018-03-20 14:13:25 -0500

    file changes

--- db/seeds.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (email, password_hash, first_name, last_name, campus, grad_date, site_link) 
-VALUES ('ajpatel@u.northwestern.edu', 'password', 'Aj', 'Pnil', 'Evanston', '2018-04-22', 'https://ayjaypnil.github.io/');
